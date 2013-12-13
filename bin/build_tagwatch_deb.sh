561e534d5f67cd8c1909b342a563bf55aab3c086
Dan Collis-Puro <djcp@thoughtbot.com>
2013-12-13 11:57:49 -0500

    Signed apt repository generation

--- bin/build_tagwatch_deb.sh
+++ bin/build_tagwatch_deb.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
-cp ./tagwatch.sh pkg-build/tagwatch/usr/bin/tagwatch
-cp ./tagwatch_all_git_repos.sh pkg-build/tagwatch/usr/bin/tagwatch_all_git_repos
+cp ./tagwatch pkg-build/tagwatch/usr/bin/tagwatch
+cp ./tagwatch_all_git_repos pkg-build/tagwatch/usr/bin/tagwatch_all_git_repos
 
 cd pkg-build
 dpkg --build tagwatch
+dpkg-sig --sign builder tagwatch.deb
 cd ..
 
-echo "tagwatch.deb has been built in pkg-build/tagwatch.deb'
+echo "tagwatch.deb has been built in pkg-build/tagwatch.deb"
+
+echo 'Building apt repo'
+
+rm -Rf apt-repo
+
+mkdir -p apt-repo/binary
+cp pkg-build/tagwatch.deb apt-repo/binary/
+
+cd apt-repo
+apt-ftparchive packages binary > Packages
+apt-ftparchive release ./ > Release
+gpg --output Release.gpg -ba Release
+
+echo "Signed apt repository built in apt-repo/"
+
+# gpg --keyserver subkeys.pgp.net --recv-keys 3BC1C2C3
+# gpg --export -a 3BC1C2C3 | apt-key add -
